98fb1af608638fd2d42aef1673db98a97c36d753
Marcin Radomski <marcin@mradomski.pl>
2017-03-04 13:43:33 +0100

    Add UB handler stub

---
+++ src/internal/undefined_behavior.c
@@ -0,0 +1,6 @@
+#include "internal/undefined_behavior.h"
+
+void __evil_ub(const char *fmt, ...)
+{
+    // TODO
+}
---
+++ src/internal/undefined_behavior.h
@@ -0,0 +1,6 @@
+#ifndef __EVILIBC_UNDEFINED_BEHAVIOR_H
+#define __EVILIBC_UNDEFINED_BEHAVIOR_H
+
+void __evil_ub(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
+#endif /* __EVILIBC_UNDEFINED_BEHAVIOR_H */
