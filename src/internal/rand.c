9e9c1e7766cd46ddaabdabcbe9c2b3bb80a7f32a
Marcin Radomski <marcin@mradomski.pl>
2017-03-11 23:38:36 +0100

    Fix formatting in internal/rand.c

--- src/internal/rand.c
+++ src/internal/rand.c
@@ -9,7 +9,7 @@ int __evil_rand(void)
 }
 
 int __evil_rand_range(int min_inclusive,
-                        int max_exclusive)
+                      int max_exclusive)
 {
     return min_inclusive + __evil_rand() % (max_exclusive - min_inclusive);
 }
