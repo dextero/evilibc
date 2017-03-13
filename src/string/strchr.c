6022ae5f66f1d9ba9559a6bc761aac95616f805e
Marcin Radomski <marcin@mradomski.pl>
2017-03-13 23:46:30 +0100

    Implement strchr

---
+++ src/string/strchr.c
@@ -0,0 +1,55 @@
+#include "string.h"
+#include "limits.h"
+#include "signal.h"
+
+#include "internal/undefined_behavior.h"
+
+char* strchr(const char* s,
+             int c)
+{
+    /*
+     * 7.24.5.2:
+     * > The strchr function locates the first occurrence of c (converted
+     * > to a char) in the string pointed to by s. The terminating null
+     * > character is considered to be part of the string.
+     *
+     * 7.1.4:
+     * > If an argument to a function has an invalid value (such as a value
+     * > outside the domain of the function, or a pointer outside the address
+     * > space of the program, or a null pointer, or a pointer to non-modifiable
+     * > storage when the corresponding parameter is not const-qualified) or
+     * > a type (after promotion) not expected by a function with variable
+     * > number of arguments, the behavior is undefined.
+     */
+    if (s == NULL) {
+        __evil_ub("passing NULL to strchr is UB: strchr(%p, %d)", s, c);
+    }
+
+#ifndef __CHAR_UNSIGNED__
+    /*
+     * 6.3.1.1:
+     * > When a value with integer type is converted to another integer type
+     * > other than _Bool, if the value can be represented by the new type,
+     * > it is unchanged.
+     * >
+     * > Otherwise, if the new type is unsigned, the value is converted by
+     * > repeatedly adding or subtracting one more than the maximum value
+     * > that can be represented in the new type until the value is in the
+     * > range of the new type.
+     * >
+     * > Otherwise, the new type is signed and the value cannot be represented
+     * > in it; either the result is implementation-defined or an
+     * > implementation-defined signal is raised.
+     *
+     * Signed integer conversions are fun.
+     */
+    if (c < CHAR_MIN || c > CHAR_MAX) {
+        __evil_idb("passing a value outside CHAR_MIN and CHAR_MAX to strchr "
+                   "when char is signed is implementation-defined: "
+                   "strchr(%p, %d)", s, c);
+        raise(SIGSEGV);
+    }
+#endif /* __CHAR_UNSIGNED__ */
+
+    return __builtin_strchr(s, c);
+}
