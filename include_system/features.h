78a91ecc94ddfa615b3d1916b20d3d4c4793497a
Marcin Radomski <marcin@mradomski.pl>
2017-09-27 22:05:37 +0200

    Add system includes so that fopen.c compiles

---
+++ include_system/features.h
@@ -0,0 +1,9 @@
+#ifndef __EVILIBC_SYSTEM_FEATURES_H
+#define __EVILIBC_SYSTEM_FEATURES_H
+
+#define __BEGIN_DECLS
+#define __END_DECLS
+#define __THROW
+#define __GNUC_PREREQ(...) 0
+
+#endif // __EVILIBC_SYSTEM_FEATURES_H
---
+++ include_system/termios.h
@@ -0,0 +1,4 @@
+#ifndef _TERMIOS_H
+#define _TERMIOS_H
+#include <bits/termios.h>
+#endif // _TERMIOS_H
