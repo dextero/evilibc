292988e02f96f03d176c168ddea774347a399488
Marcin Radomski <marcin@mradomski.pl>
2017-09-28 21:04:34 +0200

    Use stdout in hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    FILE *f = fopen("/dev/stdout", "a");
-    if (!f) {
-        return -1;
-    }
-
     const char hello[] = "Hello evilibc!\n";
-    return (int)fwrite(hello, sizeof(hello), 1, f);
+    return (int)fwrite(hello, sizeof(hello), 1, stdout);
 }
