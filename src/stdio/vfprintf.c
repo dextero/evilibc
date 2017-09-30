e20021689c63a34074c099c1148554612e74fbec
Marcin Radomski <marcin@mradomski.pl>
2017-09-30 17:31:04 +0200

    vfprintf, wip

--- src/stdio/vfprintf.c
+++ src/stdio/vfprintf.c
@@ -1,7 +1,11 @@
 #include "stdio.h"
-#include "string.h"
 
-#include "stdio/file.h"
+#include "stdbool.h"
+#include "string.h"
+#include "assert.h"
+#include "limits.h"
+
+#include "internal/undefined_behavior.h"
 
 enum flag {
     FLAG_LEFT_JUSTIFY = 1 << 0,
@@ -59,11 +63,16 @@ struct fmt {
     enum type type;
 };
 
+static int fmt_size(const struct fmt *fmt) {
+    assert(fmt->end - fmt->start <= INT_MAX);
+    return (int)(fmt->end - fmt->start);
+}
+
 static const char *parse_flags(const char *fmt,
                                int *out_flags) {
     bool flags_done = false;
     while (!flags_done) {
-        switch (*format) {
+        switch (*fmt) {
         case '-':
             *out_flags |= FLAG_LEFT_JUSTIFY;
             break;
@@ -84,10 +93,10 @@ static const char *parse_flags(const char *fmt,
             break;
         }
 
-        ++format;
+        ++fmt;
     }
 
-    return format - 1;
+    return fmt - 1;
 }
 
 static const char *parse_width(const char *fmt,
@@ -122,7 +131,7 @@ static const char *parse_precision(const char *fmt,
 
     if (*out_precision == DEFAULT) {
         /*
-         * 7.21.6.4:
+         * 7.21.6.1.4:
          * > if only the period is specified, the precision is taken as zero.
          */
         *out_precision = 0;
@@ -239,25 +248,136 @@ static const char *parse_fmt(const char *format,
     memset(out, 0, sizeof(*out));
 
     out->start = format;
-    format = parse_flags(format, out->flags);
-    format = parse_width(format, out->min_width);
-    format = parse_precision(format, out->precision);
-    format = parse_length(format, out->length);
-    format = parse_type(format, out->type);
+    format = parse_flags(format, &out->flags);
+    format = parse_width(format, &out->min_width);
+    format = parse_precision(format, &out->precision);
+    format = parse_length(format, &out->length);
+    format = parse_type(format, &out->type);
     out->end = format;
 
     return format;
 }
 
+/*
+ * 7.21.6.1.15:
+ * > The number of characters that can be produced by any single conversion
+ * > shall be at least 4095.
+ *
+ * TODO: this should probably be malloc'd
+ */
+static char g_conversion_buffer[4096];
+
+static long fetch_width(struct fmt *fmt,
+                        va_list *args) {
+    long min_width;
+
+    switch (fmt->min_width) {
+    case FROM_ARGUMENT:
+        {
+            int width_arg = va_arg(*args, int);
+            if (width_arg < 0) {
+                fmt->flags |= FLAG_LEFT_JUSTIFY;
+                min_width = -(long)width_arg;
+            } else {
+                min_width = (long) width_arg;
+            }
+        }
+        break;
+    case MISSING:
+        min_width = MISSING;
+        break;
+    case DEFAULT:
+        min_width = 0;
+        break;
+    default:
+        min_width = fmt->min_width;
+        break;
+    }
+
+    if (min_width >= (long)sizeof(g_conversion_buffer)) {
+        __evil_idb("printf does not need to support writing more than %zu bytes"
+                   " in a single %%* format specifier (%ld passed as min width)",
+                   sizeof(g_conversion_buffer) - 1, min_width);
+        min_width = (long)(sizeof(g_conversion_buffer) - 1);
+    }
+
+    assert(min_width == MISSING || min_width >= 0);
+    return min_width;
+}
+
+static int print_s(FILE *stream,
+                   const struct fmt *fmt_const,
+                   va_list *args) {
+    struct fmt fmt = *fmt_const;
+    long min_width = fetch_width(&fmt, args);
+
+    const char *str = va_arg(*args, const char *);
+    if (!str) {
+        __evil_ub("passing NULL as printf argument for %%s is UB");
+        str = "(null)";
+    }
+
+    size_t str_size;
+    if (min_width == MISSING) {
+        str_size = strlen(str);
+    } else {
+        assert(min_width >= 0);
+        // str may have a null-terminator early
+        str_size = min_width;
+        for (long i = 0; i < min_width; ++i) {
+            if (str[i] == '\0') {
+                str_size = (size_t)i;
+                break;
+            }
+        }
+    }
+
+    if (str_size > sizeof(g_conversion_buffer) - 1) {
+        __evil_idb("printf does not need to support writing more than %zu bytes"
+                   " in a single %%* format specifier (got %zu-byte string)",
+                   sizeof(g_conversion_buffer) - 1, str_size);
+        str_size = sizeof(g_conversion_buffer) - 1;
+    }
+
+    size_t pad_size = 0;
+    size_t bytes_to_write = str_size;
+    if (min_width != MISSING) {
+        assert(min_width >= 0);
+        assert(min_width < (long)sizeof(g_conversion_buffer));
+
+        /*
+         * > If the converted value has fewer characters than the field
+         * > width, it is padded with spaces (by default) on the left (or
+         * > right, if the left adjustment flag, described later, has been
+         * > given) to the field width.
+         */
+        bytes_to_write = (size_t)min_width;
+        if (str_size > bytes_to_write) {
+            bytes_to_write = str_size;
+        }
+
+        pad_size = (size_t)min_width - str_size;
+    }
+
+    if (fmt.flags & FLAG_LEFT_JUSTIFY) {
+        memcpy(&g_conversion_buffer[0], str, str_size);
+        memset(&g_conversion_buffer[str_size], ' ', pad_size);
+    } else {
+        memset(&g_conversion_buffer[0], ' ', pad_size);
+        memcpy(&g_conversion_buffer[pad_size], str, str_size);
+    }
+
+    return fwrite(g_conversion_buffer, bytes_to_write, 1, stream);
+}
+
 static int print_formatted(FILE *stream,
                            int chars_written_so_far,
                            const struct fmt *fmt,
-                           va_list args) {
+                           va_list *args) {
     switch (fmt->type) {
     case TYPE_INVALID:
-        __evil_ub("invalid format specifier: %.*s", fmt->end - fmt->start,
-                  fmt->start);
-        break;
+        __evil_ub("invalid format specifier: %.*s", fmt_size(fmt), fmt->start);
+        return -1;
     case TYPE_SIGNED_INT:
     case TYPE_UNSIGNED_INT:
     case TYPE_UNSIGNED_INT_OCTAL:
@@ -272,40 +392,68 @@ static int print_formatted(FILE *stream,
     case TYPE_DOUBLE_HEX_EXPONENTIAL_LOWER:
     case TYPE_DOUBLE_HEX_EXPONENTIAL_UPPER:
     case TYPE_INT_AS_UNSIGNED_CHAR:
+        // TODO UNIMPLEMENTED
+        return -1;
     case TYPE_STRING:
+        switch (fmt->length) {
+        case LENGTH_DEFAULT:
+            return print_s(stream, fmt, args);
+        case LENGTH_LONG:
+            // TODO UNIMPLEMENTED: print wchar_t *
+            return -1;
+        default:
+            __evil_ub("unexpected length specifier in %%s: %.*s",
+                      fmt_size(fmt), fmt->start);
+            // TODO: print fmt->start as literal
+            return -1;
+        }
     case TYPE_POINTER:
+        // TODO UNIMPLEMENTED
+        return -1;
     case TYPE_NUM_CHARS_WRITTEN:
-        *va_arg(args, int*) = chars_written_so_far;
+        if (fmt->flags != 0
+                || fmt->min_width != MISSING
+                || fmt->precision != MISSING
+                || fmt->length != LENGTH_DEFAULT) {
+            __evil_ub("specifying any flags to %%n is UB: %.*s",
+                      fmt_size(fmt), fmt->start);
+        }
+        *va_arg(*args, int*) = chars_written_so_far;
         return 0;
     case TYPE_PERCENT:
-        return _write(stream->fd, "%", 1);
+        if (fmt->flags != 0
+                || fmt->min_width != MISSING
+                || fmt->precision != MISSING
+                || fmt->length != LENGTH_DEFAULT) {
+            __evil_ub("specifying any flags to %%%% is UB: %.*s",
+                      fmt_size(fmt), fmt->start);
+        }
+        return fputc('%', stream) == EOF ? 0 : 1;
     }
+
+    // TODO: this should not be necessary
+    __builtin_unreachable();
 }
 
 int vfprintf(FILE* restrict stream,
              const char* restrict format,
              va_list args) {
     const char *segment_start = format;
+    const char *segment_end = segment_start;
     int chars_written = 0;
 
     while (*format) {
         if (*format != '%') {
             ++format;
         } else {
-            if (c != segment_start) {
-                // TODO: errors, buffering
-                int result = _write(stream->fd, segment_start, segment_end - segment_start);
-                if (result < 0) {
-                    return result; // TODO: can we proceed anyway?
-                } else {
-                    chars_written += result;
-                }
+            if (format != segment_start) {
+                chars_written += fwrite(segment_start, segment_end - segment_start, 1, stream);
             }
 
             struct fmt fmt;
             const char *fmt_end = parse_fmt(++format, &fmt);
 
-            int result = print_formatted(stream, &fmt, args);
+            int result = print_formatted(stream, chars_written, &fmt, (va_list*)&args);
             if (result < 0) {
                 return result; // TODO: can we proceed anyway?
             } else {
