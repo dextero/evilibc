d6f8d39d640d219eed5e567d08ee5dad03aa7d79
Marcin Radomski <marcin@mradomski.pl>
2017-09-30 21:42:22 +0200

    Use printf in hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    const char hello[] = "Hello evilibc!\n";
-    return (int)fwrite(hello, sizeof(hello), 1, stdout);
+    printf("1\n%s\n2%%\n3\n", "Hello evilibc!");
 }
