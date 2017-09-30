054a710b1b41500548682926ef6d32b8ea1cd7d2
Marcin Radomski <marcin@mradomski.pl>
2017-09-30 22:42:33 +0200

    Extend hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("1\n%s\n2%%\n3\n", "Hello evilibc!");
+    printf("0, default: %d\n"
+           "1, width 3: %3d\n"
+           "2, width 3, 0-padded: %03d\n"
+           "0, precision 0: %.0d\n"
+           "string: %s\n"
+           "percent: %%\n",
+           0, 1, 2, 0, "Hello evilibc!");
 }
