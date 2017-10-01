d5b684f9e5f3257d30ae783bc15ff3f35686f347
Marcin Radomski <marcin@mradomski.pl>
2017-10-01 20:38:50 +0200

    Reformat syscalls

--- os/linux-x86_64/src/syscalls.c
+++ os/linux-x86_64/src/syscalls.c
@@ -15,31 +15,31 @@
 #define _EXPAND_N(F, N, ...) EXPAND(CONCAT(F, N)(__VA_ARGS__))
 #define EXPAND_N(F, ...) _EXPAND_N(F, NARGS(__VA_ARGS__), __VA_ARGS__)
 
-#define ARGS_2(Type, Name) Type Name
-#define ARGS_4(Type, Name, ...) Type Name, ARGS_2(__VA_ARGS__)
-#define ARGS_6(Type, Name, ...) Type Name, ARGS_4(__VA_ARGS__)
-#define ARGS_8(Type, Name, ...) Type Name, ARGS_6(__VA_ARGS__)
+#define ARGS_2(Type, Name)       Type Name
+#define ARGS_4(Type, Name, ...)  Type Name, ARGS_2(__VA_ARGS__)
+#define ARGS_6(Type, Name, ...)  Type Name, ARGS_4(__VA_ARGS__)
+#define ARGS_8(Type, Name, ...)  Type Name, ARGS_6(__VA_ARGS__)
 #define ARGS_10(Type, Name, ...) Type Name, ARGS_8(__VA_ARGS__)
 #define ARGS_12(Type, Name, ...) Type Name, ARGS_10(__VA_ARGS__)
 #define ARGS(...) EXPAND_N(ARGS_, __VA_ARGS__)
 
-#define SETUP_2(Type, Name) "movq %[arg0], %%rdi\n"
-#define SETUP_4(Type, Name, ...) SETUP_2(__VA_ARGS__) "movq %[arg1], %%rsi\n"
-#define SETUP_6(Type, Name, ...) SETUP_4(__VA_ARGS__) "movq %[arg2], %%rdx\n"
-#define SETUP_8(Type, Name, ...) SETUP_6(__VA_ARGS__) "movq %[arg3], %%r10\n"
-#define SETUP_10(Type, Name, ...) SETUP_8(__VA_ARGS__) "movq %[arg4], %%r8\n"
+#define SETUP_2(Type, Name)                             "movq %[arg0], %%rdi\n"
+#define SETUP_4(Type, Name, ...)  SETUP_2(__VA_ARGS__)  "movq %[arg1], %%rsi\n"
+#define SETUP_6(Type, Name, ...)  SETUP_4(__VA_ARGS__)  "movq %[arg2], %%rdx\n"
+#define SETUP_8(Type, Name, ...)  SETUP_6(__VA_ARGS__)  "movq %[arg3], %%r10\n"
+#define SETUP_10(Type, Name, ...) SETUP_8(__VA_ARGS__)  "movq %[arg4], %%r8\n"
 #define SETUP_12(Type, Name, ...) SETUP_10(__VA_ARGS__) "movq %[arg5], %%r9\n"
 #define SETUP(...) EXPAND_N(SETUP_, __VA_ARGS__)
 
-#define REVERSE_NAMES_2(Type, Name, ...) Name
-#define REVERSE_NAMES_4(Type, Name, ...) REVERSE_NAMES_2(__VA_ARGS__), Name
-#define REVERSE_NAMES_6(Type, Name, ...) REVERSE_NAMES_4(__VA_ARGS__), Name
-#define REVERSE_NAMES_8(Type, Name, ...) REVERSE_NAMES_6(__VA_ARGS__), Name
-#define REVERSE_NAMES_10(Type, Name, ...) REVERSE_NAMES_8(__VA_ARGS__), Name
+#define REVERSE_NAMES_2(Type, Name, ...)                                 Name
+#define REVERSE_NAMES_4(Type, Name, ...)  REVERSE_NAMES_2(__VA_ARGS__),  Name
+#define REVERSE_NAMES_6(Type, Name, ...)  REVERSE_NAMES_4(__VA_ARGS__),  Name
+#define REVERSE_NAMES_8(Type, Name, ...)  REVERSE_NAMES_6(__VA_ARGS__),  Name
+#define REVERSE_NAMES_10(Type, Name, ...) REVERSE_NAMES_8(__VA_ARGS__),  Name
 #define REVERSE_NAMES_12(Type, Name, ...) REVERSE_NAMES_10(__VA_ARGS__), Name
 #define REVERSE_NAMES(...) EXPAND_N(REVERSE_NAMES_, __VA_ARGS__)
 
-#define INPUTS_1(Name) [arg0]"imr"((unsigned long)Name)
+#define INPUTS_1(Name)                             [arg0]"imr"((unsigned long)Name)
 #define INPUTS_2(Name, ...) INPUTS_1(__VA_ARGS__), [arg1]"imr"((unsigned long)Name)
 #define INPUTS_3(Name, ...) INPUTS_2(__VA_ARGS__), [arg2]"imr"((unsigned long)Name)
 #define INPUTS_4(Name, ...) INPUTS_3(__VA_ARGS__), [arg3]"imr"((unsigned long)Name)
@@ -47,11 +47,11 @@
 #define INPUTS_6(Name, ...) INPUTS_5(__VA_ARGS__), [arg5]"imr"((unsigned long)Name)
 #define INPUTS(...) EXPAND_N(INPUTS_, REVERSE_NAMES(__VA_ARGS__))
 
-#define CLOBBERS_2(Type, Name) "rdi"
-#define CLOBBERS_4(Type, Name, ...) CLOBBERS_2(__VA_ARGS__), "rsi"
-#define CLOBBERS_6(Type, Name, ...) CLOBBERS_4(__VA_ARGS__), "rdx"
-#define CLOBBERS_8(Type, Name, ...) CLOBBERS_6(__VA_ARGS__), "r10"
-#define CLOBBERS_10(Type, Name, ...) CLOBBERS_8(__VA_ARGS__), "r8"
+#define CLOBBERS_2(Type, Name)                                 "rdi"
+#define CLOBBERS_4(Type, Name, ...)  CLOBBERS_2(__VA_ARGS__),  "rsi"
+#define CLOBBERS_6(Type, Name, ...)  CLOBBERS_4(__VA_ARGS__),  "rdx"
+#define CLOBBERS_8(Type, Name, ...)  CLOBBERS_6(__VA_ARGS__),  "r10"
+#define CLOBBERS_10(Type, Name, ...) CLOBBERS_8(__VA_ARGS__),  "r8"
 #define CLOBBERS_12(Type, Name, ...) CLOBBERS_10(__VA_ARGS__), "r9"
 #define CLOBBERS(...) EXPAND_N(CLOBBERS_, __VA_ARGS__)
 
