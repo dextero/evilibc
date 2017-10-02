7e0d9f7ddca30a9d5ff7931025ff1183991cb31d
Marcin Radomski <marcin@mradomski.pl>
2017-10-02 23:45:08 +0200

    Add __evil_rand_bool

--- src/internal/rand.h
+++ src/internal/rand.h
@@ -9,10 +9,15 @@ static_assert(sizeof(int) == 4, "__evil_rand assumes 32-bit integers");
 
 int __evil_rand(void);
 int __evil_rand_range(int min_inclusive,
-                      int max_inclusive);
+                      int max_exclusive);
 int __evil_rand_negative(void);
 int __evil_rand_positive(void);
 int __evil_rand_nonzero(void);
 int __evil_rand_with_sign(int sign);
+
+static inline bool __evil_rand_bool(void)
+{
+    return __evil_rand_range(0, 2) == 1;
+}
 
 #endif /* __EVILIBC_INTERNAL_RAND_H */
