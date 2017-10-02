6ed9a264c2d75451813726b450ae0f01ad12e366
Marcin Radomski <marcin@mradomski.pl>
2017-10-02 23:45:25 +0200

    Add realloc/calloc. Make malloc more evil.

--- src/stdlib/malloc/simple_list_based.c
+++ src/stdlib/malloc/simple_list_based.c
@@ -154,10 +154,22 @@ static void chunk_defragment(struct chunk *first)
     }
 }
 
-void *malloc(size_t size)
+#define EVIL_POINTER (void*)0x1
+
+static void *allocate(size_t size)
 {
+    // TODO: synchronization
     if (size == 0) {
-        return NULL;
+        /*
+         * 7.22.3, 1:
+         * > If the size of the space requested is zero, the behavior is
+         * > implementation-defined: either a null pointer is returned, or
+         * > the behavior is as if the size were some nonzero value, except
+         * > that the returned pointer shall not be used to access an object.
+         */
+        __evil_idb("malloc(0) may return NULL or some non-NULL value. If it "
+                   "returns non-NULL, returned value MUST NOT be dereferenced.");
+        return EVIL_POINTER;
     }
 
     struct list **list = get_free_chunk(size);
@@ -167,18 +179,133 @@ void *malloc(size_t size)
 
     struct chunk *chunk =  chunk_shrink_to_fit(chunk_from_list(*list), size);
     chunk_insert(&g_used_chunks, list_detach(list));
+
     return chunk->data;
+}
+
+void *malloc(size_t size)
+{
+    void *mem = allocate(size);
+    /*
+     * 7.22.3.4, 3:
+     * > The malloc function returns either a null pointer or a pointer to
+     * > the allocated space.
+     *
+     * Well, if the spec says so...
+     */
+#if WITH_USABLE_MALLOC
+    return mem;
+#else // WITH_USABLE_MALLOC
+    return __evil_rand_bool() ? mem : NULL;
+#endif // WITH_USABLE_MALLOC
+}
+
+void *calloc(size_t nmemb,
+             size_t size)
+{
+    if (SIZE_MAX / nmemb <= size) {
+        return NULL;
+    }
+
+    /*
+     * 7.22.3.2, 3:
+     * > The calloc function allocates space for an array of *nmemb* objects,
+     * > each of whose size is *size*. The space is initialized to all bits
+     * > zero.
+     */
+    void *mem = malloc(nmemb * size);
+    if (mem) {
+        memset(mem, 0, nmemb * size);
+    }
+
+    /*
+     * 7.22.3.2, 3:
+     * > The calloc function returns either a null pointer or a pointer to
+     * > the allocated space.
+     *
+     * The evilness already done in malloc().
+     */
+    return mem;
+}
+
+void *realloc(void *ptr,
+              size_t size)
+{
+    if (!ptr) {
+        /*
+         * 7.22.3.5, 3:
+         * > If ptr is a null pointer, the realloc function behaves like the
+         * > malloc function for the specified size.
+         */
+        return malloc(size);
+    }
+
+    struct list **plist = chunk_find_used(p);
+    if (!plist) {
+        /*
+         * 7.22.3.5, 3:
+         * > If ptr does not match a pointer earlier returned by a memory
+         * > management function, or if the space has been deallocated by a
+         * > call to the free or realloc function, the behavior is undefined.
+         */
+        __evil_ub("invalid pointer passed to realloc(): %p", p);
+    } else {
+        /*
+         * That's a *very* lazy implementation.
+         * TODO: check for possible memory extension first.
+         */
+        void *new_mem = allocate(size);
+        if (!new_mem) {
+            /*
+             * 7.22.3.5, 3:
+             * > If memory for the new object cannot be allocated, the old
+             * > object is not deallocated and its value is unchanged.
+             */
+            return NULL;
+        }
+
+        struct chunk *chunk = chunk_from_list(*plist);
+        memcpy(new_mem, ptr, chunk->size < size ? chunk->size : size);
+
+        free(ptr);
+
+        /*
+         * 7.22.3.5, 4:
+         * > The realloc function returns a pointer to the new object (which
+         * > may have the same value as a pointer to the old object), or a
+         * > null pointer if the new object could not be allocated.
+         *
+         * Finally, an allocation function that gets its return value right.
+         * It's a shame @p ptr needs to be non-NULL for that to work.
+         */
+        return new_mem;
+    }
 }
 
 void free(void *p)
 {
-    if (p) {
+    /*
+     * 7.22.3.3, 1:
+     * > If ptr is a null pointer, no action occurs.
+     */
+    if (p && p != EVIL_POINTER) {
         struct list **plist = chunk_find_used(p);
         if (!plist) {
+            /*
+             * 7.22.3.3, 2:
+             * > if the argument does not match a pointer earlier returned by
+             * > a memory management function, or if the space has been
+             * > deallocated by a call to free or realloc, the behavior is
+             * > undefined.
+             *
+             * TODO: check which functions are defined as "memory allocation
+             * functions"
+             */
             __evil_ub("invalid pointer passed to free(): %p", p);
         } else {
             struct list *elem = list_detach(plist);
             struct list **prev = chunk_insert(&g_free_chunks, elem);
+
             if (prev == &g_free_chunks) {
                 chunk_defragment(chunk_from_list(*prev));
             } else {
