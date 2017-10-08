84eb3741aad0c1d06a2dba85c125647a1bea721d
Marcin Radomski <marcin@mradomski.pl>
2017-10-08 19:36:36 +0200

    Add evilibc.h

--- src/internal/undefined_behavior.c
+++ src/internal/undefined_behavior.c
@@ -1,22 +1,40 @@
 #include <evil-config.h>
 
-#include "internal/undefined_behavior.h"
-
 #include "stdarg.h"
 #include "stdio.h"
 
+#include "evilibc.h"
+
+#include "internal/undefined_behavior.h"
+
+evil_ub_handler_t *__evil_ub_handler = NULL;
+void *__evil_ub_handler_data = NULL;
+
+evil_ub_handler_t *__evil_idb_handler = NULL;
+void *__evil_idb_handler_data = NULL;
+
 void __evil_ub(const char* fmt, ...)
 {
+    if (!__evil_ub_handler) {
+        __evil_ub_handler = (evil_ub_handler_t *)vfprintf;
+        __evil_ub_handler_data = stderr;
+    }
+
     va_list list;
     va_start(list, fmt);
-    vfprintf(stderr, fmt, list);
+    __evil_ub_handler(__evil_ub_handler_data, fmt, list);
     va_end(list);
 }
 
 void __evil_idb(const char* fmt, ...)
 {
+    if (!__evil_idb_handler) {
+        __evil_idb_handler = (evil_idb_handler_t *)vfprintf;
+        __evil_idb_handler_data = stderr;
+    }
+
     va_list list;
     va_start(list, fmt);
-    vfprintf(stderr, fmt, list);
+    __evil_idb_handler(__evil_idb_handler, fmt, list);
     va_end(list);
 }
