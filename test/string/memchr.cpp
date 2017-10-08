0887135cf245ad6871a838e46e2979cde59c017a
Marcin Radomski <marcin@mradomski.pl>
2017-10-08 19:38:43 +0200

    Refactor memchr tests

--- test/string/memchr.cpp
+++ test/string/memchr.cpp
@@ -1,8 +1,10 @@
-#include <gtest/gtest.h>
+#include "Test.h"
 
 extern "C" void *test_memchr(const void *, int, size_t);
 
-TEST(memchr, found) {
+class MemchrTest : public evil::Test {};
+
+TEST_F(MemchrTest, found) {
     char buf[] = "foo bar baz";
 
     ASSERT_EQ(&buf[0], test_memchr(buf, 'f', sizeof(buf)));
@@ -11,9 +13,23 @@ TEST(memchr, found) {
     ASSERT_EQ(&buf[sizeof(buf) - 1], test_memchr(buf, '\0', sizeof(buf)));
 }
 
-TEST(memchr, not_found) {
+TEST_F(MemchrTest, not_found) {
     char buf[] = "foo bar baz";
 
     ASSERT_EQ(NULL, test_memchr(buf, 'x', sizeof(buf)));
     ASSERT_EQ(NULL, test_memchr(buf, 'b', 4));
+}
+
+TEST_F(MemchrTest, zero) {
+    ASSERT_EQ(NULL, test_memchr(" ", ' ', 0));
+
+    {
+        auto checker = evil::UBChecker{1};
+        ASSERT_EQ(NULL, test_memchr(NULL, ' ', 0));
+    }
+}
+
+TEST_F(MemchrTest, null) {
+    auto checker = evil::UBChecker{1};
+    ASSERT_EQ(NULL, test_memchr(NULL, ' ', 1));
 }
