8898d4c14feba204d90a60d4108f38e0d4fd9cff
Marcin Radomski <marcin@mradomski.pl>
2017-10-15 19:23:06 +0200

    Fix rand tests

--- test/internal/rand.cpp
+++ test/internal/rand.cpp
@@ -12,51 +12,43 @@ class RandTest : public evil::Test {};
 
 TEST_F(RandTest, uniform_enough) {
     array<size_t, UINT16_MAX + 1> buckets{};
+    static const size_t LIMIT = (UINT16_MAX + 1) * 1000;
+    //static const size_t LIMIT = (size_t)INT_MAX + 1;
 
-    for (size_t i = 0 ; i < (UINT16_MAX * 1000);  ++i) {
-        ++buckets[__evil_rand() % (UINT16_MAX + 1)];
+    for (size_t i = 0 ; i < LIMIT;  ++i) {
+        int val = __evil_rand();
+        ++buckets[val % (UINT16_MAX + 1)];
     }
 
     /* arbitrarily chosen, +-150 */
     auto minmax = minmax_element(begin(buckets), end(buckets));
-    ASSERT_LT(*minmax.second - *minmax.first, (size_t)300);
+    ASSERT_LE(*minmax.second - *minmax.first, (size_t)300);
 }
 
 TEST_F(RandTest, rand_range) {
-    for (size_t i = 0; i < 1024; ++i) {
-        auto min_max = minmax({__evil_rand(), __evil_rand()});
-        ASSERT_NE(min_max.first, min_max.second);
-
-        int val = __evil_rand_range(min_max.first, min_max.second);
-        ASSERT_LE(min_max.first, val);
-        ASSERT_LT(val, min_max.second);
-    }
+    int val = __evil_rand_range(10, 100);
+    ASSERT_LE(10, val);
+    ASSERT_LT(val, 100);
 }
 
 TEST_F(RandTest, rand_negative) {
-    for (size_t i = 0; i < 1024; ++i) {
-        ASSERT_GT(0, __evil_rand_negative());
-    }
+    ASSERT_GT(0, __evil_rand_negative());
 }
 
 TEST_F(RandTest, rand_positive) {
-    for (size_t i = 0; i < 1024; ++i) {
-        ASSERT_LT(0, __evil_rand_positive());
-    }
+    /* make sure next __evil_rand() returns 0 */
+    __evil_srand(2);
+    ASSERT_LT(0, __evil_rand_positive());
+    ASSERT_LT(0, __evil_rand_positive());
 }
 
 TEST_F(RandTest, rand_nonzero) {
-    for (size_t i = 0; i < 1024; ++i) {
-        ASSERT_NE(0, __evil_rand_nonzero());
-    }
+    ASSERT_NE(0, __evil_rand_nonzero());
+    ASSERT_NE(0, __evil_rand_nonzero());
 }
 
 TEST_F(RandTest, rand_with_sign) {
-    for (size_t i = 0; i < 1024; ++i) {
-        ASSERT_GT(0, __evil_rand_with_sign(__evil_rand_negative()));
-    }
-    for (size_t i = 0; i < 1024; ++i) {
-        ASSERT_LT(0, __evil_rand_with_sign(__evil_rand_positive()));
-    }
+    ASSERT_GT(0, __evil_rand_with_sign(__evil_rand_negative()));
+    ASSERT_LT(0, __evil_rand_with_sign(__evil_rand_positive()));
     ASSERT_EQ(0, __evil_rand_with_sign(0));
 }
