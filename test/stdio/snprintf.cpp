dedc4b4317f6e76b765ce7e02a5a74ac6778438b
Marcin Radomski <marcin@mradomski.pl>
2017-10-15 20:18:13 +0200

    snprintf test improvements

--- test/stdio/snprintf.cpp
+++ test/stdio/snprintf.cpp
@@ -11,60 +11,76 @@ class SnprintfTest : public evil::Test {};
 TEST_F(SnprintfTest, no_args) {
     char dst[sizeof("abc")];
 
-    ASSERT_EQ(3, test_snprintf(dst, sizeof(dst), "abc"));
-    ASSERT_EQ("abc"s, string(dst));
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "abc"));
+    EXPECT_EQ("abc"s, string(dst));
 }
 
 TEST_F(SnprintfTest, format_invalid) {
     char dst[sizeof("%?")];
     evil::UBChecker checker{1};
 
-    ASSERT_EQ(2, test_snprintf(dst, sizeof(dst), "%?"));
-    ASSERT_EQ("%?"s, string(dst));
+    EXPECT_EQ(2, test_snprintf(dst, sizeof(dst), "%?"));
+    EXPECT_EQ("%?"s, string(dst));
 }
 
 TEST_F(SnprintfTest, format_string) {
-    char dst[sizeof("abcAB")];
+    char dst[16];
 
-    ASSERT_EQ(5, test_snprintf(dst, sizeof(dst), "AB%s", "abc"));
-    ASSERT_EQ("ABabc"s, string(dst));
+    EXPECT_EQ(5, test_snprintf(dst, sizeof(dst), "AB%s", "abc"));
+    EXPECT_EQ("ABabc"s, string(dst));
 
-    ASSERT_EQ(5, test_snprintf(dst, sizeof(dst), "A%sB", "abc"));
-    ASSERT_EQ("AabcB"s, string(dst));
+    EXPECT_EQ(5, test_snprintf(dst, sizeof(dst), "A%sB", "abc"));
+    EXPECT_EQ("AabcB"s, string(dst));
 
-    ASSERT_EQ(5, test_snprintf(dst, sizeof(dst), "%sAB", "abc"));
-    ASSERT_EQ("abcAB"s, string(dst));
+    EXPECT_EQ(5, test_snprintf(dst, sizeof(dst), "%sAB", "abc"));
+    EXPECT_EQ("abcAB"s, string(dst));
+
+    EXPECT_EQ(5, test_snprintf(dst, sizeof(dst), "% 5s", "abc"));
+    EXPECT_EQ("  abc"s, string(dst));
+
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "%.3s", "abcde"));
+    EXPECT_EQ("abc"s, string(dst));
+
+    EXPECT_EQ(5, test_snprintf(dst, sizeof(dst), "% 5.3s", "abcde"));
+    EXPECT_EQ("  abc"s, string(dst));
+
+    {
+        evil::UBChecker checker{1};
+
+        EXPECT_EQ(6, test_snprintf(dst, sizeof(dst), "%s", NULL));
+        EXPECT_EQ("(null)"s, string(dst));
+    }
 }
 
 TEST_F(SnprintfTest, format_char) {
     char dst[sizeof("xAB")];
 
-    ASSERT_EQ(3, test_snprintf(dst, sizeof(dst), "AB%c", 'a'));
-    ASSERT_EQ("ABa"s, string(dst));
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "AB%c", 'a'));
+    EXPECT_EQ("ABa"s, string(dst));
 
-    ASSERT_EQ(3, test_snprintf(dst, sizeof(dst), "A%cB", 'a'));
-    ASSERT_EQ("AaB"s, string(dst));
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "A%cB", 'a'));
+    EXPECT_EQ("AaB"s, string(dst));
 
-    ASSERT_EQ(3, test_snprintf(dst, sizeof(dst), "%cAB", 'a'));
-    ASSERT_EQ("aAB"s, string(dst));
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "%cAB", 'a'));
+    EXPECT_EQ("aAB"s, string(dst));
 }
 
 TEST_F(SnprintfTest, format_char_outside_range) {
     char dst[sizeof("A")];
 
-    ASSERT_EQ(1, test_snprintf(dst, sizeof(dst), "%c", 'A' - 256));
-    ASSERT_EQ("A"s, string(dst));
+    EXPECT_EQ(1, test_snprintf(dst, sizeof(dst), "%c", 'A' - 256));
+    EXPECT_EQ("A"s, string(dst));
 
-    ASSERT_EQ(1, test_snprintf(dst, sizeof(dst), "%c", 'A' + 256));
-    ASSERT_EQ("A"s, string(dst));
+    EXPECT_EQ(1, test_snprintf(dst, sizeof(dst), "%c", 'A' + 256));
+    EXPECT_EQ("A"s, string(dst));
 }
 
 TEST_F(SnprintfTest, format_int) {
     char dst[16];
 
-    ASSERT_EQ(3, test_snprintf(dst, sizeof(dst), "%d", 123));
-    ASSERT_EQ("123"s, string(dst));
+    EXPECT_EQ(3, test_snprintf(dst, sizeof(dst), "%d", 123));
+    EXPECT_EQ("123"s, string(dst));
 
-    ASSERT_EQ(4, test_snprintf(dst, sizeof(dst), "%d", -123));
-    ASSERT_EQ("-123"s, string(dst));
+    EXPECT_EQ(4, test_snprintf(dst, sizeof(dst), "%d", -123));
+    EXPECT_EQ("-123"s, string(dst));
 }
