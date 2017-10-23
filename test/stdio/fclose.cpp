26cf9d4e71264648c5f682564cebf5e364f7b56f
Marcin Radomski <marcin@mradomski.pl>
2017-10-23 20:21:40 +0200

    Add fclose tests

---
+++ test/stdio/fclose.cpp
@@ -0,0 +1,57 @@
+#include "Test.h"
+
+using namespace ::testing;
+
+#define EVIL_EOF (-0xE0F)
+
+extern "C" FILE* test_fopen(const char* filename,
+                            const char* mode);
+extern "C" int test_fwrite(const void *data,
+                           size_t size,
+                           size_t nmemb,
+                           FILE *f);
+extern "C" int test_fclose(FILE *f);
+
+class FcloseTest : public evil::Test {};
+
+TEST_F(FcloseTest, null) {
+    evil::UBChecker ub{1};
+    EXPECT_EQ(EVIL_EOF, test_fclose(nullptr));
+}
+
+TEST_F(FcloseTest, failing_close) {
+    EXPECT_CALL(_syscalls, _isatty(_))
+        .WillOnce(Return(1));
+    EXPECT_CALL(_syscalls, _access(_, _))
+        .WillOnce(Return(0));
+    EXPECT_CALL(_syscalls, _open(_, _, _))
+        .WillOnce(Return(3));
+
+    FILE *f = test_fopen("", "r");
+    EXPECT_NE(nullptr, f);
+
+    EXPECT_CALL(_syscalls, _close(3))
+        .WillOnce(Return(-1));
+
+    EXPECT_EQ(EVIL_EOF, test_fclose(f));
+}
+
+TEST_F(FcloseTest, failing_flush) {
+    char heap[4096];
+    EXPECT_CALL(_syscalls, _isatty(_))
+        .WillOnce(Return(0));
+    EXPECT_CALL(_syscalls, _sbrk(_))
+        .WillOnce(Return(heap));
+    EXPECT_CALL(_syscalls, _open(_, _, _))
+        .WillOnce(Return(3));
+
+    FILE *f = test_fopen("", "w");
+    test_fwrite("a", 1, 1, f);
+
+    EXPECT_CALL(_syscalls, _write(_, _, _))
+        .WillOnce(Return(-1));
+    EXPECT_CALL(_syscalls, _close(3))
+        .WillOnce(Return(0));
+
+    EXPECT_EQ(EVIL_EOF, test_fclose(f));
+}
