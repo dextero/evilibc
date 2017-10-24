ed16ce94936816566eafad1075d5a7425f7a563e
Marcin Radomski <marcin@mradomski.pl>
2017-10-24 23:39:07 +0200

    Make mocked functions take std::string to allow == compare

--- test/SyscallsMock.h
+++ test/SyscallsMock.h
@@ -21,11 +21,11 @@ public:
 
     static SyscallsMock *instance() { return _instance_stack.top(); }
 
-    MOCK_METHOD2(_access, int(const char *path, int mode));
+    MOCK_METHOD2(_access, int(const std::string &path, int mode));
     MOCK_METHOD1(_close,  int(int fd));
     MOCK_METHOD1(_isatty, int(int fd));
     MOCK_METHOD2(_kill,   int(long pid, int sig));
-    MOCK_METHOD3(_open,   int(const char *path, int flags, int mode));
+    MOCK_METHOD3(_open,   int(const std::string &path, int flags, int mode));
     MOCK_METHOD0(_getpid, long(void));
     MOCK_METHOD3(_write,  ssize_t(int fd, const void *buf, size_t count));
     MOCK_METHOD1(_sbrk,   void *(ptrdiff_t increment));
