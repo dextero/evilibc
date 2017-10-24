bd2ae7a17328200cdfaea4dfcab44e7b183abeb2
Marcin Radomski <marcin@mradomski.pl>
2017-10-24 23:57:14 +0200

    Fix FflushTest.short_write

--- test/stdio/fflush.cpp
+++ test/stdio/fflush.cpp
@@ -62,7 +62,7 @@ TEST_F(FflushTest, failed_write) {
 
 TEST_F(FflushTest, short_write) {
     const char data[] = "foo";
-    EXPECT_EQ(sizeof(data), test_fwrite(data, sizeof(data), 1, _f));
+    EXPECT_EQ(sizeof(data), test_fwrite(data, 1, sizeof(data), _f));
 
     EXPECT_CALL(_syscalls, _write(3, _, 4))
         .WillOnce(Return(2));
